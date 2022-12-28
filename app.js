509a66b6b5f42a23dc26a549506260f1c4ae1df9
radiumls <zl123405@163.com>
2022-12-28 22:16:25 +0800

    add send static file

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const Koa = require('koa');
+const send = require('koa-send');
 const app = new Koa();
 
+const PAGE_PREFIX_PATH = '/pages';
+const STATIC_PREFIX_PATH = '/static';
+const WEB_STATIC_LOCATION = './build';
 // response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+// XXX: 所有的接口请求都必须放置在静态文件服务之前
+app.use(async ctx => {
+  // react编译后的静态文件的地址
+  if (ctx.path.startsWith(STATIC_PREFIX_PATH)){
+    await send(ctx, ctx.path, {
+      root: WEB_STATIC_LOCATION,
+      index: 'index.html',
+    });
+  } else {
+    // react的页面访问入口文件
+    await send(ctx, `${WEB_STATIC_LOCATION}/index.html`);
+  }
 });
 
-app.listen(3000);
+app.listen(13948);
