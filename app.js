cebcf7977afcd60970fc8d669da9a7394c11dea8
radiumls <zl123405@163.com>
2024-01-04 21:17:47 +0800

    chore: 增加lpsim的入口

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PAGE_PREFIX_PATH = '/pages';
 const STATIC_PREFIX_PATH = '/static';
 const WEB_STATIC_LOCATION = './build';
 const OSS_DOMAIN = 'http://static.7shengzhaohuan.online';
+const LPSIM_PREFIX_PATH = '/lpsim';
+const LPSIM_API_PREFIX = '/api';
+const LPSIM_LOCATION = './lpsim';
+const LPSIM_DOMAIN = 'http://localhost:8000';
 // response
 // XXX: 所有的接口请求都必须放置在静态文件服务之前
 app.use(async ctx => {
@@ -16,6 +20,17 @@ app.use(async ctx => {
     //   root: WEB_STATIC_LOCATION,
     //   index: 'index.html',
     // });
+  } else if(ctx.path.startsWith(LPSIM_PREFIX_PATH)){
+    const path = ctx.path.replace(LPSIM_PREFIX_PATH, '');
+    if(path.startsWith(LPSIM_API_PREFIX)){
+      const apiPath = path.replace(LPSIM_API_PREFIX, '');
+      ctx.redirect(`${LPSIM_DOMAIN}${apiPath}`);
+    } else if(path === '/' || path === ''){
+      await send(ctx, `${LPSIM_LOCATION}/index.html`);
+    } else {
+      // 有点hack, 后面还是上cdn吧……
+      await send(ctx, `${LPSIM_LOCATION}/lpsim/${path}`);
+    }
   } else {
     // react的页面访问入口文件
     await send(ctx, `${WEB_STATIC_LOCATION}/index.html`);
